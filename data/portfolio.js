92100dee08e2ba6b0919b1c3ebda6d7ab45ac64d
Paul Heading <paulheading@me.com>
2022-09-20 11:23:30 +0100

    portfolio links updated

--- data/portfolio.js
+++ data/portfolio.js
@@ -1,67 +1,206 @@
-const path = (src) => `/portfolio/stephanie-cannon-audiobook-narration-${src}-cover-art.jpeg`
-
-const link = (src, href) => ({ src: path(src), href })
+const path = (title) => `/portfolio/${title}`
 
 const portfolio = [
-  link(`3rd-person-charlotte-winters-series-thriller-girl-under-water`, `https://www.google.com`),
-  link(`3rd-person-charlotte-winters-series-thriller-watch-her-sleep`, `https://www.google.com`),
-  link(`3rd-person-thriller-charlotte-winters-series-first-girl-gone`, `https://www.google.com`),
-  link(`science-fiction-bluebird`, `https://www.google.com`),
-  link(`3rd-person-historical-fiction-marion-kummerow-a-light-in-the-window`, `https://www.google.com`),
-  link(`historical-fiction-marion-kummerow-from-the-dark-we-rise`, `https://www.google.com`),
-  link(`marion-kummerow-the-girl-in-the-shadows`, `https://www.google.com`),
-  link(`non-fiction-reality-no-reality`, `https://www.google.com`),
-  link(`historical-fiction-not-without-my-sister`, `https://www.google.com`),
-  link(`3rd-person-riley-sager-thrillers-final-girls`, `https://www.google.com`),
-  link(`3rd-person-riley-sager-thrillers-lock-every-door`, `https://www.google.com`),
-  link(`3rd-person-riley-sager-thrillers-the-last-time-i-lied`, `https://www.google.com`),
-  link(`young-adult-one`, `https://www.google.com`),
-  link(`apex`, `https://www.google.com`),
-  link(`black-widows`, `https://www.google.com`),
-  link(`blackwood`, `https://www.google.com`),
-  link(`buried`, `https://www.google.com`),
-  link(`childrens-fiction-lucas-lost-voice`, `https://www.google.com`),
-  link(`childrens-fiction-on-the-shelf`, `https://www.google.com`),
-  link(`childrens-ya-the-great-unexpected`, `https://www.google.com`),
-  link(`everybody-lies`, `https://www.google.com`),
-  link(`fiction-contemporary-the-origins-of-iris`, `https://www.google.com`),
-  link(`fiction-southern-1st-person-south-carolina-the-note`, `https://www.google.com`),
-  link(`hes-at-your-door`, `https://www.google.com`),
-  link(`historical-fiction-flight-girls`, `https://www.google.com`),
-  link(`how-it-ends`, `https://www.google.com`),
-  link(`indecent`, `https://www.google.com`),
-  link(`kids-fiction-angry-birds-stella-diaries-dahlia-and-the-ultimate-invention`, `https://www.google.com`),
-  link(`kids-fiction-angry-birds-stella-diaries-poppy-and-the-prank-too-far`, `https://www.google.com`),
-  link(`kids-fiction-angry-birds-stella-diaries-stella-has-enough`, `https://www.google.com`),
-  link(`kids-fiction-angry-birds-stella-diaries-willow-takes-the-stage`, `https://www.google.com`),
-  link(`kids-fiction-stella-has-enough`, `https://www.google.com`),
-  link(`landing`, `https://www.google.com`),
-  link(`love-between-the-lines`, `https://www.google.com`),
-  link(`monsoon-season`, `https://www.google.com`),
-  link(`my-daughters-mistake`, `https://www.google.com`),
-  link(`navel-gazing`, `https://www.google.com`),
-  link(`non-fiction-getting-it-done-when-youre-depressed`, `https://www.google.com`),
-  link(`romance-promises-at-indigo-bay`, `https://www.google.com`),
-  link(`science-fiction-dreams-the-sarah-midnight-trilogy`, `https://www.google.com`),
-  link(`science-fiction-dreamspinner`, `https://www.google.com`),
-  link(`science-fiction-the-poison-eater`, `https://www.google.com`),
-  link(`somebody-to-love`, `https://www.google.com`),
-  link(`strange-truth`, `https://www.google.com`),
-  link(`suspense-one-night-new-york`, `https://www.google.com`),
-  link(`the-boys-club`, `https://www.google.com`),
-  link(`the-coral-bride`, `https://www.google.com`),
-  link(`the-fall-of-deadworld`, `https://www.google.com`),
-  link(`the-marriage-lie`, `https://www.google.com`),
-  link(`the-marriage-secret`, `https://www.google.com`),
-  link(`the-night-before`, `https://www.google.com`),
-  link(`the-one-who-wrote-destiny`, `https://www.google.com`),
-  link(`the-holiday-swap`, `https://www.google.com`),
-  link(`thriller-a-knock-at-the-door`, `https://www.google.com`),
-  link(`ya-lgbtq-pulp`, `https://www.google.com`),
-  link(`zombies-at-tiffanies`, `https://www.google.com`),
-  link(`zombies-in-new-york`, `https://www.google.com`),
-  link(`the-bench`, `https://www.google.com`),
-  link(`historical-fiction-the-lost-mother`, `https://www.google.com`),
+  {
+    href: `https://www.audible.co.uk/pd/The-Origins-of-Iris-Audiobook/1529357705`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-fiction-contemporary-the-origins-of-iris.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/The-Fall-of-Deadworld-Audiobook/1786182912?qid=1649795670&sr=1-3&ref=a_search_c3_lProduct_1_3&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=KVPH6JJEC2SFP9MG0QTH`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-the-fall-of-deadworld-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.storytel.com/sg/en/books/stella-has-enough-1024725`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-kids-fiction-angry-birds-stella-diaries-stella-has-enough-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/One-Night-New-York-Audiobook/B09NCF758S?qid=1652725886&sr=1-1&ref=a_search_c3_lProduct_1_1&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=WWK2M3W0FJ2M8Q1F2SQB`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-suspense-one-night-new-york-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/The-One-Who-Wrote-Destiny-Audiobook/B07N96GDWF?qid=1649796646&sr=1-36&ref=a_search_c3_lProduct_1_36&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=9SD9PV5G1507TSVZXA9W`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-the-one-who-wrote-destiny-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Stargazer-Audiobook/B09ZFPHD5Q?qid=1652728779&sr=1-5&ref=a_search_c3_lProduct_1_5&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=E8GSH19FDJYJSQJ1R3CS`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-stargazer-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/The-Flight-Girls-Audiobook/1405545747?qid=1649799007&sr=1-1&ref=a_search_c3_lProduct_1_1&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=GATE0AHW33TGV6GX3JHW`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-historical-fiction-flight-girls-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Branding-That-Means-Business-Audiobook/B0B5LXK575?qid=1662806605&sr=1-1&ref=a_search_c3_lProduct_1_1&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=M1MAWE3MT2ENMQHJS4HA`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-branding-that-means-business-non-fiction-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Black-Widows-Audiobook/1409196992?qid=1649795670&sr=1-16&ref=a_search_c3_lProduct_1_16&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=KVPH6JJEC2SFP9MG0QTH`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-black-widows-cover-art.jpg`),
+  },
+  {
+    href: `https://www.storytel.com/sg/en/books/dreamspinner-s01e01-where-dreamers-meet-1546860`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-science-fiction-dreamspinner-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.storytel.com/in/en/books/on-the-shelf-1539551`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-childrens-fiction-on-the-shelf-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Mindwalker-Audiobook/B0B5B3GQ4R?qid=1660392494&sr=1-2&ref=a_search_c3_lProduct_1_2&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=0JJ6XF34FRBZV5BC0CTN`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-mindwalker-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Pulp-Audiobook/1488205388?qid=1652727798&sr=1-7&ref=a_search_c3_lProduct_1_7&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=SVFNN157KRVTVV4B1ABH`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-ya-lgbtq-pulp-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Not-Without-My-Sister-Audiobook/B08YP1K6C9?qid=1649799043&sr=1-1&ref=a_search_c3_lProduct_1_1&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=JXC66X2FDMF3PXV9VJXZ`,
+    src: path(`stephanie-cannon-us-audiobook-historical-fiction-narrator-not-without-my-sister-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/The-Great-Unexpected-Audiobook/B00AQKH4N0?qid=1652727566&sr=1-1&ref=a_search_c3_lProduct_1_1&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=8TJ2E998S258TBRXMYEC`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-childrens-ya-the-great-unexpected-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Apex-Audiobook/B010QMZWEA?qid=1649795670&sr=1-10&ref=a_search_c3_lProduct_1_10&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=KVPH6JJEC2SFP9MG0QTH`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-apex-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/One-Audiobook/B013PEJNLO?qid=1649799195&sr=1-2&ref=a_search_c3_lProduct_1_2&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=HQRQ90BFX06AE9PK0JBX`,
+    src: path(`stephanie-cannon-us-audiobook-narration-young-adult-one-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/The-Lost-Mother-Audiobook/B08TF6T52M?qid=1649798978&sr=1-1&ref=a_search_c3_lProduct_1_1&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=WKSN4BWYDST56XWEQ90S`,
+    src: path(`stephanie-cannon-us-narrator-the-lost-mother-cover-art-historical-fiction-audiobook.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Bluebird-Audiobook/B09RTQ662N?qid=1652728212&sr=1-2&ref=a_search_c3_lProduct_1_2&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=88A39NC4BDDPTBD76BQ2`,
+    src: path(`stephanie-cannon-audiobook-narrator-science-fiction-bluebird-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Final-Girls-Audiobook/B0742GKGQR?qid=1649798726&sr=1-1&ref=a_search_c3_lProduct_1_1&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=QCCGREWKDDMGP0J4GQ5G`,
+    src: path(`stephanie-cannon-us-audiobook-narration-3rd-person-riley-sager-thrillers-final-girls-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/My-Big-Fake-Wedding-Audiobook/B0B7R386PK?qid=1660392494&sr=1-3&ref=a_search_c3_lProduct_1_3&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=0JJ6XF34FRBZV5BC0CTN`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-my-big-fake-wedding-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Girl-Under-Water-Audiobook/B08PZKLB4W?ref=a_series_Ch_c4_lProduct_1_2&pf_rd_p=3a993200-e281-426d-b228-794441551983&pf_rd_r=KM7146KBFZJG798MYX57`,
+    src: path(`stephanie-cannon-audiobook-narration-3rd-person-charlotte-winters-series-thriller-girl-under-water-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Getting-It-Done-When-Youre-Depressed-Second-Edition-Audiobook/0744045827?qid=1652728677&sr=1-1&ref=a_search_c3_lProduct_1_1&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=JT0ENGM3FWM7TVPN85DE`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-non-fiction-getting-it-done-when-youre-depressed-cover-art.jpg`),
+  },
+  {
+    href: `https://www.storytel.com/sg/en/books/willow-takes-the-stage-1037050`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-kids-fiction-angry-birds-stella-diaries-willow-takes-the-stage-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/The-Child-of-Ukraine-Audiobook/B0B5Y8PW6H?qid=1660392494&sr=1-6&ref=a_search_c3_lProduct_1_6&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=0JJ6XF34FRBZV5BC0CTN`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-child-of-ukraine-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Blood-Sisters-Audiobook/B09X8B41HX?qid=1660392494&sr=1-5&ref=a_search_c3_lProduct_1_5&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=0JJ6XF34FRBZV5BC0CTN`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-blood-sisters-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.storytel.com/sg/en/books/reality-no-reality-1056101`,
+    src: path(`stephanie-cannon-non-fiction-us-audiobook-narrator-cover-art-reality-no-reality.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/My-Daughters-Mistake-Audiobook/B09G72LR7Y?qid=1649795670&sr=1-5&ref=a_search_c3_lProduct_1_5&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=KVPH6JJEC2SFP9MG0QTH`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-my-daughters-mistake-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/The-Note-Audiobook/B09V8CFVZS?qid=1652728779&sr=1-6&ref=a_search_c3_lProduct_1_6&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=E8GSH19FDJYJSQJ1R3CS`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-fiction-southern-1st-person-south-carolina-the-note-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/The-Bleeding-Audiobook/B09ZBHS7GW?qid=1660392494&sr=1-1&ref=a_search_c3_lProduct_1_1&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=0JJ6XF34FRBZV5BC0CTN`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-the-bleeding-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Everybody-Lies-Audiobook/B08KTTS2YL?qid=1652729213&sr=1-29&ref=a_search_c3_lProduct_2_9&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=3W4Q0WWD3KQ97D90QATZ`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-everybody-lies-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.storytel.com/sg/en/books/dahlia-and-the-ultimate-invention-1037051`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-kids-fiction-angry-birds-stella-diaries-dahlia-and-the-ultimate-invention-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/The-Boys-Club-Audiobook/1409193551?qid=1649795670&sr=1-19&ref=a_search_c3_lProduct_1_19&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=KVPH6JJEC2SFP9MG0QTH`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-the-boys-club-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Lock-Every-Door-Audiobook/1473570301?ref=a_author_Ri_c9_lProduct_1_2&pf_rd_p=af5062e9-57de-425c-9e02-6d8ad006b9aa&pf_rd_r=S15MJE4CD6GS8ZKKNHD0`,
+    src: path(`stephanie-cannon-us-audiobook-narration-3rd-person-riley-sager-thrillers-lock-every-door-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/First-Girl-Gone-Audiobook/B08HR2Q2SD?ref=a_series_Ch_c4_lProduct_1_1&pf_rd_p=3a993200-e281-426d-b228-794441551983&pf_rd_r=KM7146KBFZJG798MYX57`,
+    src: path(`stephanie-cannon-audiobook-narration-3rd-person-thriller-charlotte-winters-series-first-girl-gone-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/The-Holiday-Swap-Audiobook/1529356385?qid=1649796646&sr=1-10&ref=a_search_c3_lProduct_1_10&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=9SD9PV5G1507TSVZXA9W`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-thee-holiday-swap-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Not-Without-My-Sister-Audiobook/B08YP1K6C9?qid=1649799043&sr=1-1&ref=a_search_c3_lProduct_1_1&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=JXC66X2FDMF3PXV9VJXZ`,
+    src: path(`stephanie-cannon-us-audiobook-historical-fiction-narrator-not-without-my-sister-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/The-Coral-Bride-Audiobook/1399108689?qid=1652728779&sr=1-19&ref=a_search_c3_lProduct_1_19&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=E8GSH19FDJYJSQJ1R3CS`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-the-coral-bride-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/The-Night-Before-Audiobook/1409190056?qid=1649796646&sr=1-34&ref=a_search_c3_lProduct_1_34&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=9SD9PV5G1507TSVZXA9W`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-the-night-before-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/How-It-Ends-Audiobook/1405543736?qid=1649796646&sr=1-35&ref=a_search_c3_lProduct_1_35&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=9SD9PV5G1507TSVZXA9W`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-how-it-ends-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Indecent-Audiobook/B07B1236M7?qid=1649796646&sr=1-40&ref=a_search_c3_lProduct_1_40&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=9SD9PV5G1507TSVZXA9W`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-indecent-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/The-Cabin-in-the-Woods-Audiobook/B09YFFB4Z7?qid=1660392494&sr=1-8&ref=a_search_c3_lProduct_1_8&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=0JJ6XF34FRBZV5BC0CTN`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-the-cabin-in-the-woods-cover-art.jpg`),
+  },
+  {
+    href: `https://www.storytel.com/in/en/books/lucas-lost-voice-1037055`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-childrens-fiction-lucas-lost-voice-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Strange-Things-Done-Audiobook/B077BL7Y65?qid=1652729837&sr=1-1&ref=a_search_c3_lProduct_1_1&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=YJHKP1ZQH2SMBYBB8VHK`,
+    src: path(`-crop image to square-stephanie-cannon-us-audiobook-narrator-thriller-strange-things-done-cover-art.jpeg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/The-Bench-Audiobook/1405545615?qid=1649795670&sr=1-14&ref=a_search_c3_lProduct_1_14&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=KVPH6JJEC2SFP9MG0QTH`,
+    src: path(`stephanie-cannon-us-narrator-the-bench-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Zombies-in-New-York-and-Other-Bloody-Jottings-Audiobook/B009OLISLO?qid=1649797474&sr=1-69&ref=a_search_c3_lProduct_2_19&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=CS22AYB0C7ZNDH2WJ4EY`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-zombies-in-new-york.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Whisper-of-the-Seals-Audiobook/B09ZBKT3FC?qid=1660392494&sr=1-4&ref=a_search_c3_lProduct_1_4&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=0JJ6XF34FRBZV5BC0CTN`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-whisper-of-the-seals-cover-art.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/Promises-at-Indigo-Bay-Audiobook/B08L876BPB?qid=1652728526&sr=1-1&ref=a_search_c3_lProduct_1_1&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=A4E1EKSYBZKJDF9CQNQD`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-romance-promises-at-indigo-bay.jpg`),
+  },
+  {
+    href: `https://www.audible.co.uk/pd/A-Knock-at-the-Door-Audiobook/1405542438?qid=1649796646&sr=1-30&ref=a_search_c3_lProduct_1_30&pf_rd_p=c6e316b8-14da-418d-8f91-b3cad83c5183&pf_rd_r=9SD9PV5G1507TSVZXA9W`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-thriller-a-knock-at-the-door-cover-art.jpg`),
+  },
+  {
+    href: `https://www.storytel.com/sg/en/books/poppy-and-the-prank-too-far-1030079`,
+    src: path(`stephanie-cannon-us-audiobook-narrator-kids-fiction-angry-birds-stella-diaries-poppy-and-the-prank-too-far-cover-art.jpeg`),
+  },
 ]
 
 export default portfolio
