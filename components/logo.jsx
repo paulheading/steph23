342ad873f2c9af0cef58af01943b71dd8ade1ad2
Paul Heading <paulheading@me.com>
2022-11-04 09:39:52 +0000

    logo test

--- components/logo.jsx
+++ components/logo.jsx
@@ -1,13 +1,15 @@
 import styles from 'styles/components/logo.module.scss'
-import { VO } from 'logos'
+import useMediaQuery from 'hooks/useMediaQuery'
+import { StephDesktop, StephMobile } from 'logos'
 
 export function Logo({ className }) {
+  const isDesktop = useMediaQuery(`(min-width: 520px)`)
   const customClass = className ? className : ''
 
   return (
     <div className={customClass}>
       <div className={styles.container}>
-        <VO className={styles.svg} />
+        {isDesktop ? <StephDesktop className={styles.svg} /> : <StephMobile className={styles.svg} />}
         <div className={styles.shadow}></div>
       </div>
     </div>
