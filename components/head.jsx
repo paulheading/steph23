df49c0326cce0decbe29755f864e5d0bf1530c22
Paul Heading <paulheading@me.com>
2023-06-03 09:35:06 +0100

    twitter metadata

--- components/head.jsx
+++ components/head.jsx
@@ -1,13 +1,20 @@
 import NextHead from 'next/head'
+import { home } from 'data/seo'
 
 export function Head({ title, keywords, desc }) {
+  title = title ? title : 'Stephanie Cannon / Home page / Female Voiceover Artist'
   return (
     <NextHead>
-      <title>{title ? title : 'Paul Heading | Front End Dev | Portfolio'}</title>
+      <title>{title}</title>
       {keywords && <meta name="keywords" content={keywords} />}
       {desc && <meta name="description" content={desc} />}
-      <meta name="author" content="Paul Heading" />
+      <meta name="author" content="Stephanie Cannon" />
       <link rel="icon" href="/favicon.png" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@sparkthecannon" />
+      <meta name="twitter:title" content={home.title} />
+      <meta name="twitter:description" content={home.desc} />
+      <meta name="twitter:image" content="/twitter_image.jpg" />
     </NextHead>
   )
 }
